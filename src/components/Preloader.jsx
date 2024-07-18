1e1b7e037731011bcf19255db59e66a55f93580d
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2024-07-18 15:23:52 +0500

    worked on logic in new component

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { FaYinYang } from "react-icons/fa6";
 
-export default class Preloader extends Component {
-  render() {
-    return (
-      <div>
-        <FaYinYang className="mx-auto size-10 animate-spin text-red-500" />
-      </div>
-    );
-  }
-}
+const Preloader = () => {
+  return (
+    <div>
+      <FaYinYang className="mx-auto size-10 animate-spin text-red-500" />
+    </div>
+  );
+};
+
+export default Preloader;
