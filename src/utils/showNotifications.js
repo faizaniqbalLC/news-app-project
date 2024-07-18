ada6697a8f3c9434823a8a16319e236e4b61b7c0
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2024-07-18 14:55:49 +0500

    worked on toaster

--- src/utils/showNotifications.js
+++ src/utils/showNotifications.js
@@ -2,7 +2,7 @@ import { toast } from "react-toastify";
 
 const showNotification = (type, message) => {
   const style = {
-    position: "top-right",
+    position: "bottom-right",
     autoClose: 5000,
     hideProgressBar: false,
     closeOnClick: true,
